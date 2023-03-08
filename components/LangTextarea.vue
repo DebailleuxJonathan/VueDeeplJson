7fae2d94b0322b72fc592fb36e949bf651662d92
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2023-03-08 23:49:05 +0100

    Ui > add color and shadow to Ui

--- components/LangTextarea.vue
+++ components/LangTextarea.vue
@@ -64,22 +64,22 @@ const copy = (text: string) => {
           :placeholder="props.placeholder"
           v-model="value"
           type="json"
-          class="w-full h-full border border-gray-200 rounded-md p-1"
+          class="w-full h-full border border-gray-200 rounded-md py-1.5 pl-3 shadow"
       />
       <div class="absolute top-0 right-0 flex flex-col gap-2 p-2">
         <div class="flex gap-2 w-full justify-end items-center">
           <p v-if="isCopy" class="text-green-500">Texte copié !</p>
-          <button class="w-max p-2 border border-gray-200 rounded-md cursor-pointer" @click="copy(value)">
+          <button class="w-max p-2 bg-amber-500 hover:bg-amber-600 text-white rounded-md cursor-pointer shadow" @click="copy(value)">
             <ClipboardIcon class="w-5 h-5"/>
           </button>
         </div>
         <div class="flex gap-2 w-full justify-end items-center">
           <p v-if="isDownload" class="text-green-500">Texte téléchargé !</p>
           <button
-              class="w-max p-2 border border-gray-200 rounded-md cursor-pointer"
+              class="w-max p-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer shadow"
               @click="download"
               :disabled="value === ''"
-              :class="value === '' && 'bg-gray-50 text-gray-400 cursor-not-allowed'"
+              :class="value === '' && '!bg-gray-50 !text-gray-400 cursor-not-allowed'"
           >
             <ArrowDownTrayIcon class="w-5 h-5"/>
           </button>
