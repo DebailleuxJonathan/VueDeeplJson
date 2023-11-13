5d88bb8baa73c3a537c8cfd35a7ec523b2046398
DebailleuxJonathan <jonathandebailleux@yahoo.fr>
2023-11-13 14:51:37 +0100

    Fix call language deepl

--- server/api/languages/index.ts
+++ server/api/languages/index.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (e) => {
     const method = e.node.req.method
 
     if (method === "GET") {
-        const languages: any = await $fetch('https://api-free.deepl.com/v2/languages?type=target', {
+        const languages: any = await $fetch('https://api.deepl.com/v2/languages?type=target', {
             method: "get", params: {
                 auth_key: process.env.DEEPL_AUTH_KEY,
             }
