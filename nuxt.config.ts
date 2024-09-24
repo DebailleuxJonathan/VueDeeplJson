4d5e148e5a9310b4da85df0fffa28f9e4ab761a5
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2024-09-24 14:12:49 +0200

    init shadc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,40 @@
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/i18n',
-        '@nuxtjs/color-mode',
-        '@nuxt/test-utils/module',
-        '@nuxt/ui',
-        '@nuxtjs/supabase'
-    ],
-    supabase: {
-        redirect: false
-    },
-    colorMode: {
-        classSuffix: '',
-    },
-    css: ['~/assets/css/main.css'],
-    i18n: {
-        vueI18n: './i18n.config.ts'
-    },
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    '@nuxt/test-utils/module',
+    '@nuxt/ui',
+    '@nuxtjs/supabase',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ],
+
+  supabase: {
+      redirect: false
+  },
+
+  colorMode: {
+      classSuffix: '',
+  },
+
+  i18n: {
+      vueI18n: './i18n.config.ts'
+  },
+
+  tailwindcss: {
+      cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+      configPath: 'tailwind.config',
+      exposeConfig: {
+          level: 2
+      },
+      config: {},
+      viewer: true,
+  },
+
+  shadcn: {
+      prefix: '',
+      componentDir: './components/ui'
+  },
+
+  compatibilityDate: '2024-09-24'
 })
